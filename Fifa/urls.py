2e5ddfa7cfda3d42a9788b1407e467871f16fe44
Eduardo Acha <eduardoacha@gmail.com>
2014-12-20 19:21:01 -0300

    Created placeholders for some functions

---
+++ Fifa/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from Fifa import views
+
+urlpatterns = patterns('',
+                       url(r'^league/new/', views.new_league, name='new_league'),
+                       url(r'^player/new/(?P<league_id>\d+)/', views.new_player, name='new_player'),
+                       url(r'^match/generate/(?P<league_id>\d+)/', views.generate_match, name='generate_match'),
+)
--- Fifa/views.py
+++ Fifa/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def new_league(request):
+    return HttpResponse("New League")
+
+
+def new_player(request, league_id):
+    return HttpResponse("New Player")
+
+
+def generate_match(request, league_id):
+    return HttpResponse("Generate Match")
