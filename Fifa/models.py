4976a77e5940d266985bffd95852e5ae8eb77ee2
Eduardo Acha <eduardoacha@gmail.com>
2014-12-20 19:03:03 -0300

    Created League, Player and Match models

--- Fifa/models.py
+++ Fifa/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class League(models.Model):
+    name = models.CharField(max_length=100)
+    start = models.BooleanField(default=False)
+    finish = models.BooleanField(default=False)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    league = models.ForeignKey(League)
+
+
+class Match(models.Model):
+    league = models.ForeignKey(League)
+    local = models.ForeignKey(Player, related_name='match_locals')
+    visit = models.ForeignKey(Player, related_name='match_visits')
+    week = models.IntegerField()
+    local_score = models.IntegerField()
+    visit_score = models.IntegerField()
