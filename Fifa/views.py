44714a3a1150d6900e8beba7002f41046439fd28
Eduardo Acha <eduardoacha@gmail.com>
2014-12-23 09:56:36 -0300

    Added player form template

--- Fifa/views.py
+++ Fifa/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from Fifa.forms import LeagueForm
+from django.shortcuts import render, get_object_or_404
+from Fifa.forms import LeagueForm, PlayerForm
+from Fifa.models import League, Player
 
 
 def new_league(request):
@@ -15,7 +16,19 @@ def new_league(request):
 
 
 def new_player(request, league_id):
-    return HttpResponse("New Player")
+    #return HttpResponse("New Player")
+    league = get_object_or_404(League, id=league_id)
+    if request.POST:
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            player = form.save(commit=False)
+            player.league = league
+            player.save()
+
+            return HttpResponseRedirect('/')
+    else:
+        form = PlayerForm()
+    return render(request, 'fifa/player_form.html', {'form': form, 'league': league})
 
 
 def generate_match(request, league_id):
