163246be4131e5bbaadeef204df7719d90174bfa
Eduardo Acha <eduardoacha@gmail.com>
2014-12-23 00:57:21 -0300

    Created Player Form

--- Fifa/forms.py
+++ Fifa/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
-from Fifa.models import League
+from Fifa.models import League, Player
 
 
 class LeagueForm(ModelForm):
 
     class Meta:
         model = League
-        fields = ['name',]
+        fields = ['name']
+
+
+class PlayerForm(ModelForm):
+
+    class Meta:
+        model = Player
+        fields = ['name', 'team']
