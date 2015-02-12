54f0ee4404a6b5d1d5ef53d529fbb7203b70c929
milenkotomic <miletomc@gmail.com>
2015-02-12 00:27:50 -0300

    Nuevo requeriment
    Integración de Twitter :D

--- Fifa/views.py
+++ Fifa/views.py
@@ -257,7 +257,7 @@ def generate_match(league, matches_per_week, start_week, matches_between):
         league.total_matches = matches
         league.save()
 
-def set_result(request, match_id):  
+def set_result(request, match_id):
     if request.POST:
         # Setting the match result
         match = get_object_or_404(Match, id=match_id)
