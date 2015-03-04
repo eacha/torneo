07446cc4d2992db74459875a47f16542aa0b7475
milenkotomic <miletomc@gmail.com>
2015-03-04 18:14:48 -0300

    New element to admin

--- Fifa/admin.py
+++ Fifa/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Fifa.models import League, Player, Match, PositionTable, RegistrationLeague, Team, Week
+from Fifa.models import League, Player, Match, PositionTable, RegistrationLeague, Team, Week, LeaguePlayer
 
 admin.site.register(League)
 
@@ -9,3 +9,5 @@ admin.site.register(Player)
 admin.site.register(RegistrationLeague)
 admin.site.register(Team)
 admin.site.register(Week)
+admin.site.register(LeaguePlayer)
+
