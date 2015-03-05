638ab9e41c8897f658ec79a7642f7fe576f62b15
milenkotomic <miletomc@gmail.com>
2015-03-05 00:57:59 -0300

    Borrada url no usada

--- Fifa/urls.py
+++ Fifa/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
                        url(r'^logout/', views.logout_view, name='logout'),
                        url(r'^register/', views.register, name='register'),
 
-                       url(r'^match/generate/(?P<league_id>\d+)/', views.generate_match, name='generate_match'),
                        url(r'^match/result/(?P<match_id>\d+)/', views.set_result, name='set_match_result'),
 
                        url(r'^cover/', views.cover, name='cover'),
