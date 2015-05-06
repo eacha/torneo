4111c8118599941569cfda523e3bc8c70ee66ecf
milenkotomic <miletomc@gmail.com>
2015-05-06 10:23:58 -0300

    Security Fix

--- Torneo/settings.py
+++ Torneo/settings.py
@@ -117,7 +117,7 @@ SERVER_EMAIL = 'torneosdcc@gmail.com'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'torneosdcc'
-EMAIL_HOST_PASSWORD = 'TorneosDCC0702'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
